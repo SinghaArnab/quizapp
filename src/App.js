aba86f1e994ca72554e33949d3461d2310106df4
ArnabSingha <Arnabsingha9004@gmail.com>
2023-03-05 21:23:54 +0530

    quiz done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import BmiCalculator from './BMICalculator/BmiCalculator';
-import Footer from './Footer/Footer';
+import  Quizcard from './Quiz/Quizcard'
+import Footer from './Footer/Footer'
 
 function App() {
   return (
     <div className="App">
-      <BmiCalculator/>
-      <Footer/>
+     <Quizcard/>
+     <Footer/>
+     
     </div>
   );
 }
